1955b36da7fa01c790e1cfdacabb64f32e06e353
BGaspar2 <brayan202gaspar@hotmail.com>
2026-07-24 10:10:36 -0500

    fix: agregar testMatch en playwright.config.ts para detectar archivos .js en tests/

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   testDir: path.join(__dirname, 'tests'),
+  testMatch: '**/*.js',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
