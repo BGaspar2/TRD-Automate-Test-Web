7616f768285965a34887b4f52008b6f81266f87d
BGaspar2 <brayan202gaspar@hotmail.com>
2026-07-24 10:13:01 -0500

    fix: aumentar timeout de prueba a 120s en playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 export default defineConfig({
   testDir: path.join(__dirname, 'tests'),
   testMatch: '**/*.js',
+  timeout: 120000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
