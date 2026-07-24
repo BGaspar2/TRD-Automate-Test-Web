4d95441df0f3149c1e9b50ddf797bb7024b837d0
BGaspar2 <brayan202gaspar@hotmail.com>
2026-07-24 10:19:20 -0500

    chore: remove webkit configuration from playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,10 +21,6 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    }
   ],
 });
